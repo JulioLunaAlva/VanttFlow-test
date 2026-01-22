dee632bc8b0865b06bb99eaee68af133859528c5
JulioLunaAlva <cluna4887@gmail.com>
2026-01-21 19:54:17 -0600

    Fix: Remove merge conflicts from config files

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
     plugins: {
         tailwindcss: {},
         autoprefixer: {},
     },
 }
-=======
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-}
->>>>>>> 5514d7732ec911852e1fd69f7bbfd09383699edc
